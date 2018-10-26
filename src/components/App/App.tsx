bd6566017436aa3b5151cf911c818468f2d18dc6
Sasha Moak <sasha.moak@gmail.com>
2018-10-26 16:39:56 -0700

    Slight rearranging of furniture

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+       <CssBaseline />
+      </div>
+    );
+  }
+}
+
+export default App;
