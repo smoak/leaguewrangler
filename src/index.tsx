ce5da776de556f87a7a52101a561090d8b991f2a
Sasha Moak <sasha.moak@gmail.com>
2019-03-29 11:31:38 -0700

    Adds apollo and initial client configuration

--- src/index.tsx
+++ src/index.tsx
@@ -6,18 +6,43 @@ import { Provider } from 'react-redux';
 import createStore from './data/store/createStore';
 import { setSessionUserToken } from './data/domains/sessionSetUserToken/actions';
 import { getUserToken } from './support/auth';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 
 const store = createStore();
 const userToken = getUserToken();
+
+const httpLink = createHttpLink({
+  uri: 'https://teamcowboy-graphql.gigalixirapp.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: userToken ? `Bearer ${userToken}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 if (userToken) {
   store.dispatch(setSessionUserToken(userToken));
 }
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
