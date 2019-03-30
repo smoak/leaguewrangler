0f94b1d1c5d8a0314b92a6c30e3d2490113aa976
Sasha Moak <sasha.moak@gmail.com>
2019-03-29 19:32:01 -0700

    Enable 100% test coverage
    And create the last remainig test to achieve 100%

---
+++ src/components/Login/styles.test.ts
@@ -0,0 +1,18 @@
+import { createMuiTheme, Theme } from '@material-ui/core/styles';
+import styles from './styles';
+
+describe('styles', () => {
+  let theme: Theme;
+
+  beforeEach(() => {
+    theme = createMuiTheme({
+      typography: {
+        useNextVariants: true,
+      },
+    });
+  });
+
+  it('generates styles correctly', () => {
+    expect(styles(theme)).toMatchSnapshot();
+  });
+});
