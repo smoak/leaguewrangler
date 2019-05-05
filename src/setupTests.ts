28883599af40ff6b947cc7b1870357ff275477cc
Sasha Moak <sasha.moak@gmail.com>
2019-05-05 16:03:10 -0700

    Using moment-timezone to force EST timezone in tests
    This allows tests to be consistent across multiple devices, Travis, etc.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,5 +2,7 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import 'jest-enzyme';
 import 'jest-localstorage-mock';
+import moment from 'moment-timezone';
 
 configure({ adapter: new Adapter() });
+moment.tz.setDefault('EST');
