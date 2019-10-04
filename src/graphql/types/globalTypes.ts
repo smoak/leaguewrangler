d680d015375aa3ee5dd07a36888439b02235543e
Sasha Moak <sasha.moak@gmail.com>
2019-10-04 09:52:05 -0700

    build(files): Adding a file that was accidentally left out
    This is being required which is causing travis builds to fail.

---
+++ src/graphql/types/globalTypes.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+/**
+ * The RSVP status of a user
+ */
+export enum RsvpStatus {
+  AVAILABLE = "AVAILABLE",
+  MAYBE = "MAYBE",
+  NO = "NO",
+  NONE = "NONE",
+  YES = "YES",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
