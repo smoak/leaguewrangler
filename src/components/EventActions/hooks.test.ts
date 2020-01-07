12b0dc028694bd0db856bce4f097b3aa7746f902
Sasha Moak <sasha.moak@gmail.com>
2020-01-07 11:32:50 -0800

    Adding missing tests

---
+++ src/components/EventActions/hooks.test.ts
@@ -0,0 +1,47 @@
+jest.mock('react-apollo');
+
+import { useMutation } from 'react-apollo';
+import { renderHook } from '@testing-library/react-hooks';
+
+import { RsvpStatus } from '../../graphql/types/globalTypes';
+import { useSaveRsvpMutation } from './hooks';
+
+describe('hooks', () => {
+  describe('.useSaveRsvpMutation', () => {
+    let saveRsvpMutation: ReturnType<typeof useSaveRsvpMutation>;
+    const eventId = 1;
+    const teamId = 1;
+
+    describe('when loading', () => {
+      beforeEach(() => {
+        (useMutation as jest.Mock).mockReturnValue([jest.fn(), { loading: true }]);
+
+        saveRsvpMutation = renderHook(() => useSaveRsvpMutation({ eventId, teamId })).result.current;
+      });
+
+      it('returns the correct loading value', () => {
+        expect(saveRsvpMutation.loading).toBe(true);
+      });
+
+      it('returns a saveRsvpCallback', () => {
+        expect(saveRsvpMutation.saveRsvpCallback).toBeDefined();
+      });
+    });
+
+    describe('when the `saveRsvpCallback` is called', () => {
+      let saveRsvp: jest.Mock;
+
+      beforeEach(() => {
+        saveRsvp = jest.fn();
+        (useMutation as jest.Mock).mockReturnValue([saveRsvp, { loading: false }]);
+
+        saveRsvpMutation = renderHook(() => useSaveRsvpMutation({ eventId, teamId })).result.current;
+        saveRsvpMutation.saveRsvpCallback(RsvpStatus.NONE);
+      });
+
+      it('calls `saveRsvp` with the correct variables', () => {
+        expect(saveRsvp).toHaveBeenCalledWith({ variables: { eventId, teamId, rsvpStatus: RsvpStatus.NONE } });
+      });
+    });
+  });
+});
