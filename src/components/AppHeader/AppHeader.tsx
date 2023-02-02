aae1e4a92fda50ff95d11a61ab969d520f9705ed
Sasha Moak <sasha.moak@gmail.com>
2023-02-01 20:22:11 -0800

    feat: update AppHeader
    Also try to use an env variable to spit out the app version

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -2,36 +2,18 @@ import { Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
 import Toolbar from '@mui/material/Toolbar';
-import { WithStyles } from '@mui/styles';
-import createStyles from '@mui/styles/createStyles';
-import withStyles from '@mui/styles/withStyles';
 import MenuIcon from '@mui/icons-material/Menu';
-import { FC } from 'react';
 
 import UserAvatar from '../UserAvatar';
 
-const styles = createStyles({
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-});
-
-export const AppHeader: FC<WithStyles<typeof styles>> = ({ classes }) => {
+export const AppHeader = () => {
   return (
     <AppBar position="static">
       <Toolbar>
-        <IconButton
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="Menu"
-          size="large">
+        <IconButton edge="start" color="inherit" aria-label="Menu" sx={{ mr: 2 }} size="large">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" color="inherit" className={classes.grow}>
+        <Typography variant="h6" color="inherit" sx={{ flexGrow: 1 }}>
           {process.env.REACT_APP_WEBSITE_NAME}
         </Typography>
         <UserAvatar />
@@ -40,4 +22,4 @@ export const AppHeader: FC<WithStyles<typeof styles>> = ({ classes }) => {
   );
 };
 
-export default withStyles(styles)(AppHeader);
+export default AppHeader;
